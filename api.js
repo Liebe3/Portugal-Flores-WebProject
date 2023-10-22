32ddb7d816899fdbcd7d3fef8219b36d69547fe3
Jallen Portugal <jallenqweqwe123@gmail.com>
2023-10-22 22:54:40 +0800

    API finished
    API doneee

--- api.js
+++ api.js
@@ -23,3 +23,75 @@ function handleSearch(searchTerm) {
     setError("Please fill out this field");
   }
 }
+
+searchInput.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    const searchTerm = searchInput.value.trim();
+    handleSearch(searchTerm);
+  }
+});
+
+async function searchMovies(title) {
+  try {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    if (data.Response === "True") {
+      setMovies(data.Search);
+      setError("");
+    } else {
+      setMovies([]);
+    }
+  } catch (error) {
+    setError("No result.");
+  }
+}
+
+function setMovies(movies) {
+  movieContainer.innerHTML = "";
+
+  if (movies.length > 0) {
+    movies.forEach((movie, index) => {
+      const movieDiv = document.createElement("div");
+      movieDiv.className = "movie";
+
+      const yearDiv = document.createElement("div");
+      const yearParagraph = document.createElement("p");
+      yearParagraph.textContent = movie.Year;
+      yearDiv.appendChild(yearParagraph);
+      movieDiv.appendChild(yearDiv);
+
+      const imageDiv = document.createElement("div");
+      const image = document.createElement("img");
+      image.src =
+        movie.Poster !== "N/A"
+          ? movie.Poster
+          : "https://via.placeholder.com/400";
+      image.alt = movie.Title;
+      imageDiv.appendChild(image);
+      movieDiv.appendChild(imageDiv);
+
+      const infoDiv = document.createElement("div");
+      const typeSpan = document.createElement("span");
+      typeSpan.textContent = movie.Type;
+      const titleHeader = document.createElement("h3");
+      titleHeader.textContent = movie.Title;
+      infoDiv.appendChild(typeSpan);
+      infoDiv.appendChild(titleHeader);
+      movieDiv.appendChild(infoDiv);
+
+      movieContainer.appendChild(movieDiv);
+    });
+  } else {
+    const emptyDiv = document.createElement("div");
+    emptyDiv.className = "empty";
+    const h2 = document.createElement("h2");
+    h2.textContent = "No movies Found";
+    emptyDiv.appendChild(h2);
+    movieContainer.appendChild(emptyDiv);
+  }
+}
+
+function setError(errorMessage) {
+  errorDiv.textContent = errorMessage;
+}
